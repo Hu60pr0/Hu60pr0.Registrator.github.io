f1253294dee5eae73f75ea8d6623792e3c902770
Hu60pr0 <150838171+Hu60pr0@users.noreply.github.com>
2024-09-13 17:31:56 +0200

    Add files via upload

---
+++ docs/node.js
@@ -0,0 +1,27 @@
+document.getElementById('registerForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const nameInput = document.getElementById('name').value;
+    const passwordInput = document.getElementById('password').value;
+
+    if (nameInput === 'Hugo' && passwordInput === '123456') {
+        document.getElementById('message').textContent = 'Registro exitoso';
+        document.getElementById('message').style.color = 'green';
+
+
+
+    } else {
+        document.getElementById('message').textContent = 'Nombre o contraseña incorrectos';
+        document.getElementById('message').style.color = 'red';
+    }
+});
+
+// Mostrar/ocultar contraseña
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+
+    // Cambiar el texto del botón dependiendo del estado
+    this.textContent = type === 'password' ? 'Mostrar' : 'Ocultar';
+});
